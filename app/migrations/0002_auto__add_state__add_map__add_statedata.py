3d4aacce728a2c5db78ac7bd326c43ec5be33de7
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2014-04-21 21:08:22 -0400

    initial map models

---
+++ app/migrations/0002_auto__add_state__add_map__add_statedata.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'State'
+        db.create_table(u'app_state', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
+            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('abbreviation', self.gf('django.db.models.fields.CharField')(max_length=8)),
+        ))
+        db.send_create_signal(u'app', ['State'])
+
+        # Adding model 'Map'
+        db.create_table(u'app_map', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
+            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='maps', null=True, to=orm['account.User'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=1024)),
+        ))
+        db.send_create_signal(u'app', ['Map'])
+
+        # Adding model 'StateData'
+        db.create_table(u'app_statedata', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
+            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('state_map', self.gf('django.db.models.fields.related.ForeignKey')(related_name='states', to=orm['app.Map'])),
+            ('state', self.gf('django.db.models.fields.related.ForeignKey')(related_name='dataset', to=orm['app.State'])),
+            ('data', self.gf('django.db.models.fields.CharField')(max_length=64)),
+        ))
+        db.send_create_signal(u'app', ['StateData'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'State'
+        db.delete_table(u'app_state')
+
+        # Deleting model 'Map'
+        db.delete_table(u'app_map')
+
+        # Deleting model 'StateData'
+        db.delete_table(u'app_statedata')
+
+
+    models = {
+        u'account.user': {
+            'Meta': {'object_name': 'User'},
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'app.map': {
+            'Meta': {'object_name': 'Map'},
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'maps'", 'null': 'True', 'to': u"orm['account.User']"})
+        },
+        u'app.state': {
+            'Meta': {'object_name': 'State'},
+            'abbreviation': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'app.statedata': {
+            'Meta': {'object_name': 'StateData'},
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
+            'data': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'dataset'", 'to': u"orm['app.State']"}),
+            'state_map': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'states'", 'to': u"orm['app.Map']"})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['app']
--- app/models.py
+++ app/models.py
@@ -1,4 +1,32 @@
 from django.db import models
 
 from model_utils.models import TimeStampedModel
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Map(TimeStampedModel):
+    user = models.ForeignKey(User, related_name='maps', null=True)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+
+    def __unicode__(self):
+        return self.title
+
+
+class State(TimeStampedModel):
+    name = models.CharField(max_length=128)
+    abbreviation = models.CharField(max_length=8)
+
+    def __unicode__(self):
+        return self.name
+
+
+class StateData(TimeStampedModel):
+    state_map = models.ForeignKey(Map, related_name='data')
+    state = models.ForeignKey(State, related_name='dataset')
+    data = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return "%s Data for %s" % (self.state.name, self.state_map)
